38a8a62c009d442e10369b2338a24831e02ec17c
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2022-12-10 02:51:33 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = '0';
+
+	for (; i <= '10'; i++)
+		putchar(i);
+
+	putchar("\n");
+
+	return (0);
+}
