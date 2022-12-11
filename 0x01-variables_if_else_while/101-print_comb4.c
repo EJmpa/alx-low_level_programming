38b53954e66aa4194797c5a6d355cef72a6c4382
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2022-12-11 09:22:16 -0800

    A  program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		for (digit_2 = digit_1 + 1; digit_2 < 8; digit_2++)
 		{
-			for (digit_3 = digit_1 + 2; digit_3 <= 9; digit_3++)
+			for (digit_3 = digit_2 + 1; digit_3 <= 9; digit_3++)
 			{
 				putchar((digit_1 % 10) + '0');
 
