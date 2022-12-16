b59239ba2eef1849f64d318774a0be2f46d4c1ea
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2022-12-16 04:32:17 -0800

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Displays the output of multiplication of two inegers
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return output of the 'mul' function
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
