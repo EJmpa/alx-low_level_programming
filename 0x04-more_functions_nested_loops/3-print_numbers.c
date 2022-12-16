8ff7b28a50abc0dee87f747a478f78c4590bb940
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2022-12-16 04:54:20 -0800

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: Sequence of numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n')
+}
