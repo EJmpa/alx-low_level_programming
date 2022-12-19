c4f0337e707144a8f6ac5f10fb7bacb0ee365643
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2022-12-19 12:22:46 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a
+ * pointer to an int as parameter and
+ * updates the value it points to to 98.
+ * @n: Integer to be changed
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
