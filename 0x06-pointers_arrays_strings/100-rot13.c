06e233df5e927370d18a03ad373c799263fc8f98
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2022-12-22 07:07:59 -0800

    Completed rest of the task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: A string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+
+				break;
+			}
+
+			s[a] += 13;
+
+			break;
+
+		}
+
+		a++;
+	}
+
+	return (s);
+}
