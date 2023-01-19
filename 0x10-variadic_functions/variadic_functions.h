93a28f9b739e670a8a50c1113a8647ff7942ea38
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-01-19 19:37:16 +0100

    Variadic Function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -23,4 +23,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /* VARIAFUNC */
+#endif /* VARIADIC_FUNCTION_h */
