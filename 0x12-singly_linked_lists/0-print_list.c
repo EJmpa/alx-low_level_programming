11410cf734ce2ba8b11680ee63b2c490efdd1e8e
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-01-27 09:28:20 +0100

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
-
 /**
- * print_list - prints all elements of a list_t list.
- * @h: a pointer to the list to iterato to
- * Return: the number of nodes
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: struct
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	size_t i = 0;
+	int count = 0;
 
-	if (h && h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-	else
-	{
-		tmp = h;
-		if (tmp)
-			tmp = h;
+	if (h == NULL)
+		return (0);
 
-		while (tmp)
+	while (h != NULL)
+	{
+		if (h->str == NULL)
 		{
-			printf("[%d] %s\n", tmp->len, tmp->str);
-			i++;
-			tmp = tmp->next;
+			printf("[%d] %s\n", 0, "(nil)");
 		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
 	}
-	return (i);
+
+	return (count);
 }
