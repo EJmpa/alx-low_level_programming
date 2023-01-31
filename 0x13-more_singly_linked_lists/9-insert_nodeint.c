46f5b89c3e7157d5236fc2654ccd02e4d5a53ea6
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-01-31 22:41:57 +0100

    Insert a node at given position (n)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,46 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - Insert a new node at given position
- * @head: head
- * @idx: index
- * @n: data
- * Return: 0
+ * insert_nodeint_at_index - thsi function create a new index
+ * @head: the head of list
+ * @idx: the index to put the new node
+ * @n: the value to put in the nex node.
+ * Description: this function create a new index
+ * section header: the header of this function is lists.h)*
+ * Return: return the final result of the sum
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	listint_t *node, *actual;
+	unsigned int i;
 
-	listint_t *new;
-	listint_t *temp;
-
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	temp = *head;
+	node = malloc(sizeof(listint_t));
 
-	while (temp != NULL)
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+
+	if (idx == '\0')
 	{
-		if (count == idx - 1)
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			else
-			{
-				new->n = n;
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-		}
-		if (temp->next != NULL)
-			temp = temp->next;
-		count++;
+		node->next = *head;
+		*head = node;
+		return (node);
 	}
+
+	actual = *head;
+
+	for (i = 0; actual; i++)
+	{
+		if (i == idx - 1)
+		{
+			node->next = actual->next;
+			actual->next = node;
+			return (node);
+		}
+		actual = actual->next;
+	}
+	free(node);
 	return (NULL);
 }
