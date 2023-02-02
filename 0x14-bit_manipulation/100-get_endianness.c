d8c4cf634775cd498026cbec23cf949ab6393e21
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-02-02 18:20:56 +0100

    Update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
- * get_endianness - get the endiannes of the arch
- * Description: get the endiannes of the arch
- * section header: the header of this function is holberton.h
- * Return: 1 in little indian - 0 in big indian.
+ * get_endianness - Entry Point
+ * Return: 0
  */
-
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *)&i;
+	unsigned int i;
 
-	if (*c)
+	i = 1;
+	if (*(char *)&i == 1)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
