851e8ca90cc4ded85f3f8f03ee8301c601d751f9
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-02-06 22:47:45 +0100

    Append text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
-  * create_file - creates a file with rw------- permissions
-  * @filename: name of the file, if NULL, return -1
-  * @text_content: contents of the file. If NULL, create an empty file
+  * append_text_to_file - appends text to a file
+  * @filename: file to append to, if NULL, return -1
+  * @text_content: content to append, if NULL, do not append
   * Return: 1 on success, -1 on failure
   */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int new_file, len, wr_stat;
+	int append_file, len, wr_stat;
 
 	if (filename == NULL)
 		return (-1);
-	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (new_file == -1)
+	append_file = open(filename, O_WRONLY | O_APPEND);
+	if (append_file == -1)
 		return (-1);
 	if (text_content == NULL)
-	{
-		close(new_file);
 		return (1);
-	}
 	for (len = 0; text_content[len]; len++)
 		;
-	wr_stat = write(new_file, text_content, len);
-	if (close(new_file) == -1)
-		return (-1);
+	wr_stat = write(append_file, text_content, len);
+	close(append_file);
 	return (wr_stat == -1 ? -1 : 1);
 }
