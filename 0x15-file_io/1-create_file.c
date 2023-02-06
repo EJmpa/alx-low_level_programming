d9b552d711f2c32f65e5573723aa144511d37c88
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-02-06 22:40:15 +0100

    Create a File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,39 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
+
 /**
- * create_file - Create a File
- * @filename: file name
- * @text_content: null terminated string to write
- * Return: 1
+ * _strlen - Returns the length of a string.
+ * @s: The string whose the length will be returned.
+ * Return: The length of s.
+ */
+int _strlen(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
+/**
+ * create_file - Creates a file.
+ * @filename: The filename
+ * @text_content: The content to write in the file.
+ * Return: 1 on success. -1 otherwise.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int fd, len = _strlen(text_content);
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
+	if (text_content)
+		write(fd, text_content, len);
+	return (len);
 
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
-	{
-		close(file);
-		return (-1);
-	}
-	else
-	{
-		write(file, text_content, i);
-	}
-
-	close(file);
-	return (1);
 }
